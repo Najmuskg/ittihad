953585744f85a77dee13c13f71a36d9d2d85c978
Niran Yousuf <niran069@gmail.com>
2025-05-20 17:44:03 +0600

    create component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
 import HeroBanner from "@/components/Hero-Banner/HeroBanner";
+import LatestSlide from "@/components/LatestSlide/LatestSlide";
 
 export default function Home() {
   return (
-    <HeroBanner/>
+    <>
+      <HeroBanner />
+      <LatestSlide />
+    </>
   );
 }
---
+++ components/LatestSlide/LatestSlide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LatestSlide() {
+	return (
+		<section>
+			<h2>Latest posts</h2>
+		</section>
+	)
+}
