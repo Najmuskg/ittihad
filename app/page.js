90f29cbf1852fb80f3e2151ac98ee4495b86929a
Sakib <najmuskg@gmail.com>
2025-05-21 10:41:11 +0600

    header modified

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import HeroBanner from "@/components/Hero-Banner/HeroBanner";
 import NewsLetter from "@/components/NewsLetter/NewsLetter";
 import LatestSlide from "@/components/LatestSlide/LatestSlide";
+import SocialMedia from "@/components/SocialMedia/SocialMedia";
 
 export default function Home() {
   return (
     <>
       <HeroBanner />
+      <SocialMedia />
       <NewsLetter />
       <LatestSlide />
     </>
--- components/Header/elements/MainNavigation.jsx
+++ components/Header/elements/MainNavigation.jsx
@@ -1,27 +1,39 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import SecondaryNavigation from "./SecondaryNavigation";
 
 export default function MainNavigation() {
+  const pathname = usePathname();
+
   const links = [
     { href: "/link1", label: "Home" },
     { href: "/link2", label: "Club" },
     { href: "/link3", label: "News & Media" },
     { href: "/link4", label: "Matches" },
   ];
+
   return (
     <>
-      <ul className="flex flex-col md:flex-row  md:space-x-[56px]">
-        {links.map(({ href, label }) => (
-          <li key={href}>
-            <Link
-              href={href}
-              className=" text-sm font-bold uppercase leading-[120%] tracking-[0.8px] hover:text-[#FFF014] transition"
-            >
-              {label}
-            </Link>
-          </li>
-        ))}
+      <ul className="flex flex-col md:flex-row md:space-x-[56px]">
+        {links.map(({ href, label }) => {
+          const isActive = pathname === href;
+
+          return (
+            <li key={href}>
+              <Link
+                href={href}
+                className={`text-sm font-bold uppercase leading-[120%] tracking-[0.8px] linkHover ${
+                  isActive ? "text-[#FFF014]" : ""
+                }`}
+              >
+                {label}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
 
       <div className="block md:hidden">
